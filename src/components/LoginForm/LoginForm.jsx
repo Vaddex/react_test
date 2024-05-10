b14a4b659396c7df99e16f0eb6f16696ad524491
Vaddex <Gosvlad236@gmail.com>
2024-05-10 15:35:43 +0300

    add form reset

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,34 @@
-import { useId } from 'react';
+import { useId, useState } from 'react';
 
 const LoginForm = () => {
+    const [values, setValues] = useState({
+        login: '',
+        password: '',
+    });
+
+    // First variant
+    // const handleLoginChange = evt => {
+    //     setValues({
+    //         ...values,
+    //         login: evt.target.value,
+    //     });
+    // };
+    // const handlePwdChange = evt => {
+    //     setValues({
+    //         ...values,
+    //         password: evt.target.value,
+    //     });
+    // };
+
+    // Second variant
+    const handleChange = evt => {
+        setValues({
+            ...values,
+            [evt.target.name]: evt.target.value,
+        });
+    };
+
+    // id
     const loginId = useId();
     const passId = useId();
 
@@ -14,18 +42,39 @@ const LoginForm = () => {
         console.log(login, password);
 
         // Значення полів
-        console.log(login.value, password.value);
+        // console.log(login.value, password.value);
 
-        // Скидаємо значення полів після відправки
-        form.reset();
+        // Значення полів 2 variant
+        console.log(values);
+
+        // First variant reset form
+        // form.reset();
+
+        // Second variant
+        setValues({
+            login: '',
+            password: '',
+        });
     };
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor={loginId}>Login</label>
-            <input type="text" name="login" id={loginId} />
+            <input
+                type="text"
+                name="login"
+                id={loginId}
+                value={values.login}
+                onChange={handleChange}
+            />
 
             <label htmlFor={passId}>Password</label>
-            <input type="password" name="password" id={passId} />
+            <input
+                type="password"
+                name="password"
+                id={passId}
+                value={values.password}
+                onChange={handleChange}
+            />
             <button type="submit">Login</button>
         </form>
     );
